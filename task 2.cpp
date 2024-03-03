9ed9a3392b24e720f35a233af69f7359d62e05aa
Lisnovetski <91295401+Bespredelkin@users.noreply.github.com>
2024-03-03 03:01:44 +0300

    Add files via upload

---
+++ task 2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+// Рассчитаем следующее число
+int WhatANumber( int number ) 
+{
+    int sum = 0;
+    while ( number > 0 ) 
+    {
+        int digit = number % 10;
+        sum += digit * digit;
+        number /= 10;
+    }
+    return sum;
+}
+
+// С помощью рекурентной функции понимаем счасливое или нет
+void HappyNumber( int number)
+{
+    // все числа вычисленные таким способом станут в итоге меньше 243
+    while (number > 243)
+    {
+        number = WhatANumber(number);
+    }
+
+    // Из теории чисел если цикл больше 243 и он не затронул 1 , то она там и не содержится
+    for ( int i = 0; i < 244; i++ )
+    {
+        if ( number == 1 )
+        {
+            std::cout << "Happy number" << std::endl;
+            exit(0);
+        }
+        number = WhatANumber(number);
+    }
+    std::cout << "Sad number" << std::endl;
+    exit(0);
+}
+
+int main()
+{
+	int number = 0;
+	std::cout << "enter a number" << std::endl;
+	std::cin >> number;
+	HappyNumber( number);
+}
