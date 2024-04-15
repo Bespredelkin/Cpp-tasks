0d49bed4c19bd7f9ae95c869a1cc4760e2da6639
Lisnovetski <91295401+Bespredelkin@users.noreply.github.com>
2024-04-15 19:03:03 +0300

    Add files via upload

---
+++ seminar 7 task 1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <algorithm>
+#include <mutex>
+
+// Мьютекс
+std::mutex mtx;
+
+// Генерация псевдослучайных чисел от 0 до 9999
+int getRandomNumber()
+{
+    return rand() % 10000;
+}
+
+
+// Функция для вычисления суммы трех максимальных элементов в массиве
+void SumOfTopThree(std::vector<int>& container, int& sum) 
+{
+    // Потокобезопасность
+    mtx.lock();
+
+    std::partial_sort(container.begin(), container.begin() + 3, container.end(), std::greater<int>());
+
+    sum = container[0] + container[1] + container[2];
+
+    mtx.unlock();
+}
+
+int main() 
+{
+    srand(time(0)); // Генерация случайных чисел теперь зависит от времени
+
+    std::vector<int> container(10000);
+
+    // Заполнение контейнера случайными числами
+    for (int i = 0; i < 10000; ++i)
+    {
+        container[i] = getRandomNumber();
+    }
+
+    int sum = 0;
+
+    // Создание потоков для параллельного вычисления
+    std::thread t1(SumOfTopThree, std::ref(container), std::ref(sum));
+    std::thread t2(SumOfTopThree, std::ref(container), std::ref(sum));
+
+    // Ожидание завершения потоков
+    t1.join();
+    t2.join();
+
+    std::cout << "Sum of the three biggest: " << sum << std::endl;
+
+    return 0;
+}
---
+++ seminar 7 task 2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <thread>
+
+// Генерация псевдослучайных чисел от 0 до 9999
+int getRandomNumber()
+{
+    return rand() % 10000;
+}
+
+
+// Функция для сортировкм массива вставкой
+void Sorting(int* array, int start, int end)
+{
+    for (int i = start + 1; i < end; ++i) 
+    {
+        int current = array[i];
+        int j = i - 1;
+        while (j >= start && array[j] > current) 
+        {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = current;
+    }
+}
+
+int main()
+{
+    srand(time(0)); // Генерация случайных чисел теперь зависит от времени
+
+    const int n = 10000; // Размер массива
+
+    int* array = new int[n];
+
+    // Заполнение контейнера случайными числами
+    for (int i = 0; i < n; ++i)
+    {
+        array[i] = getRandomNumber();
+    }
+
+    // Разделение массива на две части
+    const int middle = n / 2;
+
+    // Создание потоков для двупоточной сортировки
+    std::thread t1(Sorting, array, 0, middle);
+    std::thread t2(Sorting, array, middle, n);
+
+    // Ожидание завершения потоков
+    t1.join();
+    t2.join();
+
+    return 0;
+}
